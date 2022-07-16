2af7272c4042644ca95afdc146f7fc18f0696ad4
bbeltran22 <99772609+bbeltran22@users.noreply.github.com>
2022-07-16 10:55:36 -0600

    Initial search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
-    <div className="App mt-3 mr-3">
-      HEYYYYYY
-      <div>
-        <a href="https://github.com/bbeltran22/dictionary-project">
-          Open source
-        </a>
+    <div className="App mt-3">
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="text-center">
+          Coded By Breanna Beltran{" "}
+          <a href="https://github.com/bbeltran22/dictionary-project">
+            Open source
+          </a>
+        </footer>
       </div>
     </div>
   );
