fe1de157dab0b08f05234a32bb58dffb790839da
bbeltran22 <99772609+bbeltran22@users.noreply.github.com>
2022-07-20 12:28:32 -0600

    Added default keyword

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App mt-3">
       <div className="container">
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="home" />
         </main>
       </div>
     </div>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,36 +1,50 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Result from "./Result";
-import "./Dictionary.css";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [result, setResult] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
     setResult(response.data[0]);
   }
-  function search(event) {
-    event.preventDefault();
 
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input
-          type="search"
-          placeholder="Type a word"
-          onChange={handleKeywordChange}
-        />
-      </form>
-      <Result result={result} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <form className="text-center" onSubmit={handleSubmit}>
+          <input
+            type="search"
+            placeholder="Type a word"
+            onChange={handleKeywordChange}
+          />
+        </form>
+        <Result result={result} />
+      </div>
+    );
+  } else {
+    load();
+    return "Loading";
+  }
 }
