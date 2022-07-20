780af191a22a8446a1e279d835c10647ad6e72f1
bbeltran22 <99772609+bbeltran22@users.noreply.github.com>
2022-07-20 11:44:00 -0600

    Phonetics added

--- src/Result.js
+++ src/Result.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Result(props) {
   if (props.result) {
     return (
       <div className="Result mt-3 text-capitalize">
         <h1>{props.result.word}</h1>
+        {props.result.phonetics.map(function (phonetics, index) {
+          return (
+            <div key={index}>
+              <Phonetics phonetics={phonetics} />
+            </div>
+          );
+        })}
         {props.result.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
