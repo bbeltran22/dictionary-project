09d6d3d5f56e0cfb5cef6291391a8252b7faf403
bbeltran22 <99772609+bbeltran22@users.noreply.github.com>
2022-07-30 15:28:31 -0600

    one meaning only

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,17 +5,11 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <div className="definition">{definition.definition}</div>
-            <div className="example">
-              <em>{definition.example}</em>
-            </div>
-            <Synonyms synonyms={definition.synonyms} />
-          </div>
-        );
-      })}
+      <p>{props.meaning.definitions[0].definition}</p>
+      <p>
+        <em>{props.meaning.definitions[0].example}</em>
+      </p>
+      <Synonyms synonyms={props.meaning.definitions.synonyms} />
     </div>
   );
 }
